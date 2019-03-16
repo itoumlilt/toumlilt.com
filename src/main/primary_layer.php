fcac6666b4ea6b12b8573dd08c9e74d9631f0ed4
Ilyas Toumlilt <ilyas@toumlilt.com>
2019-03-16 18:57:30 +0100

    Update home page

--- src/main/primary_layer.php
+++ src/main/primary_layer.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * User: itoumlilt
- * Date: 25/04/2018
- * Version: 1.1
+ * Date: 16/03/2019
+ * Version: 1.2
  */
 $PATH_TO_BASE = "../../";
 
@@ -21,16 +21,18 @@ function HV_main_pl_about_me() {
             <div class="content">
                 <p>Hello World! Welcome to my personal website.</p>
                 <p>I'm currently a PhD Student in computer science
-                    at <a href="https://www.sorbonne-universite.fr/">Sorbonne Université</a>
+                    at <a href="https://www.sorbonne-universite.fr/" target="_blank">Sorbonne Université</a>
                     (previously UPMC).
-                    I am part of the <a href="https://team.inria.fr/regal/fr/">
+                    I am part of the <a href="https://team.inria.fr/delys/fr/" target="_blank">
                         Delys INRIA team</a> at Laboratoire d'Informatique de Paris 6
-                    (<a href="https://lip6.fr">LIP6</a>), under the supervision
-                    of <a href="https://pages.lip6.fr/Marc.Shapiro/">Marc Shapiro</a>.
+                    (<a href="https://lip6.fr" target="_blank">LIP6</a>), under the supervision
+                    of <a href="https://pages.lip6.fr/Marc.Shapiro/" target="_blank">Marc Shapiro</a>.
                 </p>
-                <p>My research interest is the design of distributed
-                    and operating systems, principally in consistency of
-                    partially-replicated distributed data stores.
+                <p>
+                    My research interest is the design of highly available distributed systems, principally consistency protocols in case of replication between Datacenters and Edge devices.
+                </p>
+                <p>
+                    My current thesis work is part of <a href="https://www.lightkone.eu/" target="_blank">LightKone EU Project</a>.
                 </p>
             </div><!--//content-->
         </div><!--//section-inner-->
@@ -156,16 +158,41 @@ function HV_main_pl_work_experience() {
         <div class="content">
             <div class="item">
                 <h3 class="title">PhD Student - <span class="place">Sorbonne Université, LIP6, INRIA</span> <span class="year">(Jan 2017 - Present)</span></h3>
-                <p>Design of distributed and operating systems, principally in consistency of partially-replicated distributed data stores.</p>
+                <p>
+                    Cloud-scale services improve availability and latency by geo-replicating data in several data center across the world. Nevertheless, the closest data center is often still too far away for an optimal user experience. To remain available at all times, client-side applications need to cache data at client machines. This approach is used in many recent cloud services, where developers implement caching and buffering at application level, but it doesn’t ensure system-wide consistency guarantees.
+                </p>
+                <p>
+                    My research work is on the design of highly available distributed systems, principally consistency protocols in case of replication between Datacenters and Edge Computing devices.
+                </p>
+                <p>
+                    My current work is part of EU project LightKone that aims to develop a scientifically sound and industrially validated model for doing general-purpose computation on edge networks. An edge network consists of a large set of heterogeneous, loosely coupled computing nodes situated at the logical extreme of a network. Common examples are community networks and Internet of Things networks, and networks including mobile devices, personal computers, and points of presence including Mobile Edge Computing. Internet applications are increasingly running on edge networks, to reduce latency, increase scalability, resilience, and security, and permit local decision making.
+                </p>
             </div><!--//item-->
             <div class="item">
+                <h3 class="title">Teaching Assistant - <span class="place">Sorbonne Université</span> <span class="year">(Jan 2017 - Present)</span></h3>
+                <p>
+                    I teach the following courses:<br/>
+                    - <a href="http://www-master.ufr-info-p6.jussieu.fr:8080/ue/2014/description.php?code_ue=4I405" target="_blank"> Security and System Administration (master 1).</a>(2017, 2018, 2019)<br/>
+                    - <a href="http://www-master.ufr-info-p6.jussieu.fr:8080/ue/2014/description.php?code_ue=5I453" target="_blank">Multicore Kernels and Virtualisation (master 2)</a>, memory garbage collection part. (2018, 2019)<br/>
+                    - <a href="http://www-licence.ufr-info-p6.jussieu.fr/lmd/licence/2018/ue/2I010-2019fev/" target="_blank">Introduction to operating systems (licence 2).</a> (2019)<br/>
+                    - <a href="http://www-connex.lip6.fr/~guigue/wikihomepage/pmwiki.php?n=Course.CourseLI230" target="_blank">Introduction to Java langage (licence 2).</a> (2018)
+                </p>
+            </div><!--//item-->
+
+            <div class="item">
                 <h3 class="title">Internship - <span class="place"><a href="http://www.lip6.fr/" target="_blank">LIP6, Regal Team </a></span> <span class="year">(Feb 2016 - August 2016)</span></h3>
-                <p>Provide mechanisms, within the Linux kernel, to properly and efficiently size the cache that a container has, by proposing different heuristics whose purpose is to improve the approximation of the age of the pages, at acceptable cost.</p>
+                <p>
+                    Fighting fragmentation due to virtualization in the Linux kernel: working-sets size estimating.
+                    Provide mechanisms, within the Linux kernel, to properly and efficiently size the cache available in a container, by offering different heuristics to improve page age approximation, at an acceptable cost.
+                </p>
             </div><!--//item-->
 
             <div class="item">
                 <h3 class="title">Internship - <span class="place"><a href="http://www.lip6.fr/" target="_blank">LIP6, Whisper Team </a></span> <span class="year">(June 2015 - August 2015)</span></h3>
-                <p>Optimizing the distribution of the software computation costs and applying real-time Linux patches on an embedded ship for the automobile.</p>
+                <p>
+                    Calculation and optimisation of software load distribution on an embedded automotive board.
+                    Linux Real-Time patches and Yocto recipes for SabreLite MX6 Board.
+                </p>
             </div><!--//item-->
 
             <div class="item">
